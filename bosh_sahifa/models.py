3a0f052a5aab6c2e0824513e2d8320129977bad6
Baxtiyor <bakhtiyor@gmail.com>
2025-01-26 22:01:56 +0500

    fixed urls and models

--- bosh_sahifa/models.py
+++ bosh_sahifa/models.py
@@ -44,6 +44,9 @@ class ArticleAuthor(models.Model):
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
+    def __str__(self):
+        return self.name
+
 
 
 class ArticleCategories(models.Model):
--- bosh_sahifa/urls.py
+++ bosh_sahifa/urls.py
@@ -3,13 +3,13 @@ from rest_framework.routers import DefaultRouter
 from .views import MagazineViewSet, ArticleViewSet, ArticleAuthorViewSet, ArticleCategoriesViewSet, upload_file
 
 router = DefaultRouter()
-router.register('', MagazineViewSet)
-router.register('articles', ArticleViewSet)
-router.register('article-authors', ArticleAuthorViewSet)
-router.register('article-categories', ArticleCategoriesViewSet)
+router.register(r'magazine', MagazineViewSet, basename='magazine')
+router.register(r'articles', ArticleViewSet, basename='articles')
+router.register(r'article-authors', ArticleAuthorViewSet, basename='article-authors')
+router.register(r'article-categories', ArticleCategoriesViewSet, basename='article-categories')
 
 urlpatterns = [
-    path('', include(router.urls)),
     path("upload_magazine_file/", upload_file, name="ck_editor_5_upload_file"),
+    path('', include(router.urls)),
 
 ]

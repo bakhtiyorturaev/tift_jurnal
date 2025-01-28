3122e5864f9d509e4b58bba27d04172dc1dbe80d
Baxtiyor <bakhtiyor@gmail.com>
2025-01-28 21:20:03 +0500

    debug=false

--- src/settings.py
+++ src/settings.py
@@ -16,8 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG') == 'True'
-DEBUG = False
+DEBUG = os.getenv('DEBUG') == 'True'
 SECRET_ADMIN_URL = os.getenv('SECRET_ADMIN_URL')
 
 ALLOWED_HOSTS = ['*']

8f189aaacf8688cea45818d83de3f657535f073f
Baxtiyor <bakhtiyor@gmail.com>
2025-02-20 20:44:47 +0500

    add tinymce form

--- ilmiy_maktablarim/admin.py
+++ ilmiy_maktablarim/admin.py
@@ -1,14 +1,7 @@
-from django import forms
 from django.contrib import admin
+from .forms import IlmiyMaktablarimForm
 from .models import IlmiyMaktablarim
-from tinymce.widgets import TinyMCE
 
-class IlmiyMaktablarimForm(forms.ModelForm):
-    haqida = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
-
-    class Meta:
-        model = IlmiyMaktablarim
-        fields = '__all__'
 
 @admin.register(IlmiyMaktablarim)
 class IlmiyMaktablarimAdmin(admin.ModelAdmin):

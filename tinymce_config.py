3b03facf8730cfcf6d7ccfc488dcae12b61aa22a
Baxtiyor <bakhtiyor@gmail.com>
2025-02-25 20:07:45 +0500

    a

--- tinymce_config.py
+++ tinymce_config.py
@@ -2,17 +2,17 @@ TINYMCE_DEFAULT_CONFIG = {
     'height': 360,
     'width': 800,
     'plugins': '''
-        advlist autolink lists link charmap print preview anchor
-        searchreplace visualblocks insertdatetime table paste
+        advlist autolink lists link image imagetools charmap print preview anchor
+        searchreplace visualblocks insertdatetime media table paste help wordcount
     ''',
     'toolbar1': '''
-        undo redo | formatselect | bold italic underline strikethrough |
-        forecolor backcolor | alignleft aligncenter alignright alignjustify |
-        bullist numlist outdent indent | preview fullscreen
+        undo redo | formatselect fontsizeselect | bold italic underline |
+        alignleft aligncenter alignright alignjustify | bullist numlist |
+        link image media | preview code fullscreen
     ''',
-    'contextmenu': 'formats',
-    'menubar': True,
-    'statusbar': True,
+    'images_upload_url': '/core/upload-image/',  # ✅ Umumiy rasm yuklash URL
+    'automatic_uploads': True,
+    'file_picker_types': 'image',
+    'images_reuse_filename': True,
+
 }
-
-

bc96db4662c5af3b068d1528e8d8c89bd9651352
Baxtiyor <bakhtiyor@gmail.com>
2025-03-02 01:26:18 +0500

    lll

--- jurnal/models.py
+++ jurnal/models.py
@@ -9,18 +9,29 @@ class AboutMagazine(models.Model):
         verbose_name="Jurnal nomi"
     )
 
-    name_uz = models.CharField(max_length=255, verbose_name="Jurnal nomi (UZ)", blank=True, null=True)
-    name_ru = models.CharField(max_length=255, verbose_name="Jurnal nomi (RU)", blank=True, null=True)
-    name_en = models.CharField(max_length=255, verbose_name="Jurnal nomi (EN)", blank=True, null=True)
-
     bio_uz = models.TextField(verbose_name="Jurnal haqida (UZ)")
     bio_ru = models.TextField(verbose_name="Jurnal haqida (RU)", blank=True, null=True)
     bio_en = models.TextField(verbose_name="Jurnal haqida (EN)", blank=True, null=True)
 
-    file_uz = models.FileField(upload_to='jurnal_haqida_fayl/', verbose_name="Fayl (UZ)", blank=True, null=True)
-    file_ru = models.FileField(upload_to='jurnal_haqida_fayl/', verbose_name="Fayl (RU)", blank=True, null=True)
-    file_en = models.FileField(upload_to='jurnal_haqida_fayl/', verbose_name="Fayl (EN)", blank=True, null=True)
+    file_uz = models.FileField(upload_to='jurnal_haqida_fayl/uz/', verbose_name="Jurnal fayli (UZ)", blank=True,
+                               null=True)
+    file_ru = models.FileField(upload_to='jurnal_haqida_fayl/ru/', verbose_name="Jurnal fayli (RU)", blank=True,
+                               null=True)
+    file_en = models.FileField(upload_to='jurnal_haqida_fayl/en/', verbose_name="Jurnal fayli (EN)", blank=True,
+                               null=True)
 
+    def name_uz(self):
+        return self.magazine.name_uz
+
+    def name_ru(self):
+        return self.magazine.name_ru
+
+    def name_en(self):
+        return self.magazine.name_en
+
+    class Meta:
+        verbose_name = "Jurnal haqida"
+        verbose_name_plural = "Jurnallar haqida"
 
     class Meta:
         verbose_name = "Jurnal haqida"

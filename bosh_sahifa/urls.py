58ddd385afdc5790377e87c975fd4960885ff942
Baxtiyor <bakhtiyor@gmail.com>
2025-03-01 22:49:02 +0500

    ccc

--- bosh_sahifa/urls.py
+++ bosh_sahifa/urls.py
@@ -7,12 +7,11 @@ router = DefaultRouter()
 router.register(r'conferences', ConferenceViewSet)  # /api/conferences/
 
 router.register(r'magazine', MagazineViewSet, basename='magazine')
-router.register(r'articles', ArticleViewSet, basename='articles')
+router.register(r'articles', ArticleViewSet, basename='article')
 router.register(r'article-authors', ArticleAuthorViewSet, basename='article-authors')
 router.register(r'article-categories', ArticleCategoriesViewSet, basename='article-categories')
 
 urlpatterns = [
-    path("upload_magazine_file/", upload_file, name="ck_editor_5_upload_file"),
     path('', include(router.urls)),
 
 ]

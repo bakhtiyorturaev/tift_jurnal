1bef5c075c538af86a93152c25d25f65fb29c830
Baxtiyor <bakhtiyor@gmail.com>
2025-03-03 22:08:23 +0500

    ...

--- jurnal/admin.py
+++ jurnal/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .forms import AboutMagazineForm, MagazineRequirementsForm
+from .forms import AboutMagazineForm#, MagazineRequirementsForm
 from .models import AboutMagazine, MagazineNews, MagazineRequirements, Statistics
 
 

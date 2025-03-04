8f0015d685c6a370ec32777cdceb9cd127b9903c
Baxtiyor <bakhtiyor@gmail.com>
2025-03-04 09:56:35 +0500

    ..

--- tahririyat/forms.py
+++ tahririyat/forms.py
@@ -3,8 +3,6 @@ from tinymce.widgets import TinyMCE
 from .models import EditorialStaff
 
 class EditorialStaffForm(forms.ModelForm):
-    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
-
     class Meta:
         model = EditorialStaff
         fields = '__all__'
